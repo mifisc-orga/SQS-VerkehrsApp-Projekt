f4b1dd560f73139c800dd9b949d212285aa0e398
Louisa Böhm <lb@4boehm.de>
2026-06-03 00:43:42 +0200

    fix frontend coverage

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import istanbul from 'vite-plugin-istanbul'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      istanbul({
+        include: 'src/*',
+        extension: ['.ts', '.tsx'],
+        requireEnv: false
+      })],
   server: {
     proxy: {
       '/api': {
