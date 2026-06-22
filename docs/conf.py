e8d722caa8ef9299210e8926e7d440f62c2272fd
Louisa Böhm <lb@4boehm.de>
2026-06-22 17:10:11 +0200

    read the docs breite

--- docs/conf.py
+++ docs/conf.py
@@ -29,4 +29,7 @@ language = 'ger'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = []
+html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
